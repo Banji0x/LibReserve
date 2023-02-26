f447a2a96ce3c11a00575996d14b9adbe052c050
Banji <87107632+Banji-07@users.noreply.github.com>
2023-02-26 23:50:50 +0100

    ...

---
+++ src/main/java/dev/banji/LibReserve/LibReserveApplication.java
@@ -0,0 +1,13 @@
+package dev.banji.LibReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/banji/LibReserve/LibReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.banji.LibReserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
