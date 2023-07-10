59d218f6d4ad7190a0ee1faaee15f3ba386a4644
Banji <87107632+Banji-07@users.noreply.github.com>
2023-07-10 23:05:10 +0100

    Security filters for the Librarian and Student that performs authentication when needed.

---
+++ src/main/java/dev/banji/LibReserve/config/filters/LibrarianAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package dev.banji.LibReserve.config.filters;
+
+import dev.banji.LibReserve.config.tokens.LibrarianAuthenticationToken;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibrarianAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private final AuthenticationConverter librarianAuthConverter;
+
+    public LibrarianAuthenticationFilter(RequestMatcher librarianPathRequestMatcher,
+                                         AuthenticationConverter librarianAuthConverter,
+                                         AuthenticationManager providerManager,
+                                         AuthenticationFailureHandler librarianAuthenticationFailureHandler,
+                                         AuthenticationSuccessHandler librarianAuthenticationSuccessHandler) {
+        super(librarianPathRequestMatcher, providerManager);
+        this.librarianAuthConverter = librarianAuthConverter;
+        this.setAuthenticationFailureHandler(librarianAuthenticationFailureHandler);
+        this.setAuthenticationSuccessHandler(librarianAuthenticationSuccessHandler);
+    }
+
+    @Override
+    protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        return super.requiresAuthentication(request, response) && !isAlreadyAuthenticated(); //authenticate if the requestMatcher matches and the user has not being previously authenticated...
+    }
+
+    protected boolean isAlreadyAuthenticated() {
+        return SecurityContextHolder.getContext().getAuthentication() != null
+                && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                && !(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)
+                && (SecurityContextHolder.getContext().getAuthentication() instanceof LibrarianAuthenticationToken);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        var unAuthenticatedLibrarianToken = (LibrarianAuthenticationToken) librarianAuthConverter.convert(request);
+        return getAuthenticationManager().authenticate(unAuthenticatedLibrarianToken);
+    }
+}
---
+++ src/main/java/dev/banji/LibReserve/config/filters/StudentAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package dev.banji.LibReserve.config.filters;
+
+import dev.banji.LibReserve.config.tokens.StudentAuthenticationToken;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private final AuthenticationConverter studentAuthConverter;
+
+    public StudentAuthenticationFilter(RequestMatcher studentPathRequestMatcher, AuthenticationConverter studentAuthConverter, AuthenticationManager providerManager, AuthenticationFailureHandler studentAuthenticationFailureHandler, AuthenticationSuccessHandler studentAuthenticationSuccessHandler) {
+        super(studentPathRequestMatcher, providerManager);
+        this.studentAuthConverter = studentAuthConverter;
+        this.setAuthenticationFailureHandler(studentAuthenticationFailureHandler);
+        this.setAuthenticationSuccessHandler(studentAuthenticationSuccessHandler);
+    }
+
+    protected boolean isAlreadyAuthenticated() {
+        return SecurityContextHolder.getContext().getAuthentication() != null
+                && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                && !(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)
+                && (SecurityContextHolder.getContext().getAuthentication() instanceof StudentAuthenticationToken);
+    }
+
+
+    @Override
+    protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        return super.requiresAuthentication(request, response) && !isAlreadyAuthenticated(); //authenticate if the requestMatcher matches and the user has not being previously authenticated...
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        var unAuthenticatedStudentToken = (StudentAuthenticationToken) studentAuthConverter.convert(request);
+        return getAuthenticationManager().authenticate(unAuthenticatedStudentToken);
+    }
+}
