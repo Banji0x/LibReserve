b4d4269829e38b469c21b504f799761543f87909
Banji <87107632+Banji-07@users.noreply.github.com>
2023-07-10 23:06:58 +0100

    Controllers and Repository.

--- src/main/java/dev/banji/LibReserve/controller/StudentController.java
+++ src/main/java/dev/banji/LibReserve/controller/StudentController.java
@@ -1,7 +1,23 @@
 package dev.banji.LibReserve.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/lib-reserve/student")
 public class StudentController {
+    @GetMapping("/") //secured with oauth2
+    @PreAuthorize("hasAuthority('SCOPE_STUDENT')")
+    public String homePage(JwtAuthenticationToken authentication) {
+        return "homePage " + authentication.getName();
+    }
+
+    @GetMapping("/welcome") //secured with oauth2
+    @PreAuthorize("hasAuthority('SCOPE_STUDENT')")
+    public String welcome(JwtAuthenticationToken authentication) {
+        return "Welcome " + authentication.getName();
+    }
 }
--- src/main/java/dev/banji/LibReserve/repository/StudentRepository.java
+++ src/main/java/dev/banji/LibReserve/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package dev.banji.LibReserve.repository;
 import dev.banji.LibReserve.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student,Long> {
-    
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByMatricNumber(String matricNumber);
 }
