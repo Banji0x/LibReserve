a0bf7c45b0d526fc72e84306a4fc3cb2e72c09ae
Banji <87107632+Banji-07@users.noreply.github.com>
2023-07-10 23:48:25 +0100

    Deleted Redundant Code.

--- src/main/java/dev/banji/LibReserve/LibReserveApplication.java
+++ src/main/java/dev/banji/LibReserve/LibReserveApplication.java
@@ -1,16 +1,12 @@
 package dev.banji.LibReserve;
 
-import dev.banji.LibReserve.config.configurationproperties.University;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
-@EnableConfigurationProperties(value = University.class)
 public class LibReserveApplication {
     public static void main(String[] args) {
         SpringApplication.run(LibReserveApplication.class, args);
     }
-
 }
