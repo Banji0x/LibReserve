69606d9b2d7e3bb137bcb5caf57e4d30b606ee9f
Banji <87107632+Banji-07@users.noreply.github.com>
2023-10-26 21:41:43 +0100

    docs: added @Setter annotation to the password field of the abstract superclass "User" .
    This generated setter will be used by the authentication providers to
    make the password field null after authentication.

--- src/main/java/dev/banji/LibReserve/model/User.java
+++ src/main/java/dev/banji/LibReserve/model/User.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Cascade;
 
 import static org.hibernate.annotations.CascadeType.ALL;
 
 @Getter
-//added for the concrete subclasses to have getter's.This is not necessary because the subclasses have direct access to the fields {protected modifier}.
+//Added for the concrete subclasses to have getter's.
+//This is not necessary because the subclasses have direct access to the fields {protected modifier}.
 @MappedSuperclass
 public abstract class User {
     protected String firstName;
@@ -18,6 +20,7 @@ public abstract class User {
     protected String gender;
 
     @Column(nullable = false)
+    @Setter
     protected String password;
     @Column(unique = true, nullable = false)
     protected String phoneNumber;
