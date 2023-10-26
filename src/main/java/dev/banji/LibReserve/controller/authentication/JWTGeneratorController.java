c54c224b2496faf2ac0d4cf0bd0e07f6e4c684e9
Banji <87107632+Banji-07@users.noreply.github.com>
2023-10-26 23:49:42 +0100

    docs(Library): Endpoint Controller that returns Jwt tokens to validated users.

--- src/main/java/dev/banji/LibReserve/controller/authentication/JWTGeneratorController.java
+++ src/main/java/dev/banji/LibReserve/controller/authentication/JWTGeneratorController.java
@@ -1,6 +1,7 @@
 package dev.banji.LibReserve.controller.authentication;
 
-import dev.banji.LibReserve.service.TokenService;
+import dev.banji.LibReserve.service.JwtTokenService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,23 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/lib-reserve/token")
-public class LoginController {
-    private final TokenService tokenService;
-
-    public LoginController(TokenService tokenService) {
-        this.tokenService = tokenService;
-    }
+@RequestMapping("/api/lib-reserve/token")
+@RequiredArgsConstructor
+public class JWTGeneratorController {
+    private final JwtTokenService tokenService;
 
     @PostMapping("/librarian")
     @PreAuthorize("hasRole('LIBRARIAN')")
-    public String welcomeLibrarian(Authentication authentication) {
+    public String librarianToken(Authentication authentication) {
         return tokenService.generateJwt(authentication);
     }
 
     @PostMapping("/student")
     @PreAuthorize("hasRole('STUDENT')")
-    public String welcomeStudent(Authentication authentication) {
+    public String studentToken(Authentication authentication) {
         return tokenService.generateJwt(authentication);
     }
 }
