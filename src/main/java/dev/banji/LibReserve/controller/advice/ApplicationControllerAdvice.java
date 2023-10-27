ca1e8bc72873f05b803cc846ca3bf9ab9ef35c1f
Banji <87107632+Banji-07@users.noreply.github.com>
2023-10-27 02:58:04 +0100

    docs(Library): Application Advice controller will catch any exception thrown during Json processing, or any library exceptions through at runtime.

--- src/main/java/dev/banji/LibReserve/controller/advice/ApplicationControllerAdvice.java
+++ src/main/java/dev/banji/LibReserve/controller/advice/ApplicationControllerAdvice.java
@@ -1,8 +1,11 @@
 package dev.banji.LibReserve.controller.advice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dev.banji.LibReserve.exceptions.LibraryRuntimeException;
 import dev.banji.LibReserve.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,4 +28,18 @@ public class ApplicationControllerAdvice {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, userNotFoundException.getMessage());
     }
 
+    @ExceptionHandler(JsonProcessingException.class)
+    public ProblemDetail jsonProcessingException(JsonProcessingException jsonProcessingException) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Error: Invalid JSON data.");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException httpMessageNotReadableException) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Error: Unable to read JSON data.");
+    }
+
+    @ExceptionHandler(LibraryRuntimeException.class)
+    ProblemDetail libraryRuntimeException(LibraryRuntimeException libraryRuntimeException) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, libraryRuntimeException.getMessage());
+    }
 }
