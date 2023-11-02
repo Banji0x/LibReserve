c024febb974e0a6c7e631302be0e8c01a6181123
Banji <87107632+Banji-07@users.noreply.github.com>
2023-11-03 00:51:58 +0100

    docs: EarlyCheckInException now has a descriptive message.

--- src/main/java/dev/banji/LibReserve/exceptions/EarlyCheckInException.java
+++ src/main/java/dev/banji/LibReserve/exceptions/EarlyCheckInException.java
@@ -1,8 +1,10 @@
 package dev.banji.LibReserve.exceptions;
 
+import java.time.LocalTime;
+
 public class EarlyCheckInException extends LibraryRuntimeException {
 
-    public EarlyCheckInException() {
-        super("");
+    public EarlyCheckInException(LocalTime timeReservationWasMadeFor) {
+        super("Early check-in are not allowed. Kindly wait till " + timeReservationWasMadeFor);
     }
 }
