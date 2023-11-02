2b521fef2d706cb56d25e16cd0f3a81057082b87
Banji <87107632+Banji-07@users.noreply.github.com>
2023-11-03 00:56:27 +0100

    docs: ReservationNotForTodayException now has a descriptive message.

--- src/main/java/dev/banji/LibReserve/exceptions/ReservationNotForTodayException.java
+++ src/main/java/dev/banji/LibReserve/exceptions/ReservationNotForTodayException.java
@@ -1,24 +1,8 @@
 package dev.banji.LibReserve.exceptions;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class ReservationNotForTodayException extends LibraryRuntimeException {
-    private ReservationNotForTodayException(LocalDate reservedForDate) {
-        super("Reservation is not valid. It is valid for the " + reservedForDate);
+    private ReservationNotForTodayException() {
+        super("Reservation is not valid for today.");
     }
 
-    private ReservationNotForTodayException(String message) {
-        super(message);
-    }
-
-    public static ReservationNotForTodayException dateFormatter(LocalDate reservedForDate) {
-        var customFormatter = DateTimeFormatter.ofPattern("dd,MM,yyyy"); // "dd,MM,yyyy" format
-        reservedForDate.format(customFormatter);
-        return new ReservationNotForTodayException(reservedForDate);
-    }
-
-    public static ReservationNotForTodayException ReservationCannotBeForTodayException() {
-        return new ReservationNotForTodayException("Proposed Booking is not for today");
-    }
 }
