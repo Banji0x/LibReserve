1fa2f3da1b71ebaef31683108b52a05ac2546c81
Banji <87107632+Banji-07@users.noreply.github.com>
2023-11-05 14:08:52 +0100

    refactor(Library): blacklistJwt to blacklistAccessToken.

--- src/main/java/dev/banji/LibReserve/service/JwtTokenService.java
+++ src/main/java/dev/banji/LibReserve/service/JwtTokenService.java
@@ -1,15 +1,13 @@
 package dev.banji.LibReserve.service;
 
 import dev.banji.LibReserve.config.userDetails.StudentSecurityDetails;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwsHeader;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -21,32 +19,22 @@ import static java.time.temporal.ChronoUnit.MINUTES;
 @RequiredArgsConstructor
 public class JwtTokenService {
     private final JwtEncoder encoder;
-    private final List<String> jwtTokenBlackList;
+    private final List<Jwt> jwtTokenBlackList;
     @Value("${jwt.expirationTime}")
     private Long expirationTime;
 
-    public String generateJwt(Authentication authentication) {
-        return generateJwt(authentication, this.expirationTime);
+    public String generateAccessToken(Authentication authenticatedToken) {
+        return generateAccessToken(authenticatedToken, this.expirationTime);
     }
 
-    public String generateJwt(Authentication authenticatedToken, Long expirationTime) {
-        var scope = authenticatedToken
-                .getAuthorities()
-                .stream()
-                .map(GrantedAuthority::getAuthority).toString();
-        JwtClaimsSet claims = JwtClaimsSet
-                .builder()
-                .issuedAt(Instant.now())
-                .issuer("LibReserve")
-                .expiresAt(Instant.now().plus(expirationTime, MINUTES))
-                .subject(((StudentSecurityDetails) authenticatedToken.getPrincipal()).getMatricNumber())
-                .claim("scope", scope.trim())
-                .build();
+    public String generateAccessToken(Authentication authenticatedToken, @NonNull Long expirationTime) {
+        var scope = authenticatedToken.getAuthorities().stream().map(GrantedAuthority::getAuthority).toString();
+        JwtClaimsSet claims = JwtClaimsSet.builder().issuedAt(Instant.now()).issuer("LibReserve").expiresAt(Instant.now().plus(expirationTime, MINUTES)).subject(((StudentSecurityDetails) authenticatedToken.getPrincipal()).getMatricNumber()).claim("scope", scope.trim()).build();
         JwtEncoderParameters encoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS512).build(), claims);
         return encoder.encode(encoderParameters).getTokenValue();
     }
 
-    public boolean blacklistJwt(String jwt) {
+    public boolean blacklistAccessToken(Jwt jwt) {
         return jwtTokenBlackList.add(jwt);
     }
 }
