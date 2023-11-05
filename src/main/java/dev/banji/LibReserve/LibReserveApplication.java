47b68003dea143a96807292112fb7ed62c3540f9
Banji <87107632+Banji-07@users.noreply.github.com>
2023-11-05 13:05:27 +0100

    ""

--- src/main/java/dev/banji/LibReserve/LibReserveApplication.java
+++ src/main/java/dev/banji/LibReserve/LibReserveApplication.java
@@ -3,7 +3,6 @@ package dev.banji.LibReserve;
 import dev.banji.LibReserve.config.properties.LibraryConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
