c4d2ce0cf2100b42c23eda3104dce3c5cea30ed9
Banji <87107632+Banji-07@users.noreply.github.com>
2023-11-30 06:02:51 +0100

    bug: the "jwtAuthenticationToken" can be null if the "BearerTokenAuthenticationFilter" can't resolve a bearer token from the request. fix: a null check is now present, and an  AuthenticationCredentialsNotFoundException object is now thrown to indicate that a token cannot be found. An AccessDeniedException object is also now thrown if the jwt token is in the blacklist.

--- src/main/java/dev/banji/LibReserve/config/filters/JwtAccessTokenBlacklistAuthenticationFilter.java
+++ src/main/java/dev/banji/LibReserve/config/filters/JwtAccessTokenBlacklistAuthenticationFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -21,14 +23,13 @@ public class JwtAccessTokenBlacklistAuthenticationFilter extends OncePerRequestF
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var authenticatedJwt = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        Jwt jwtToken = authenticatedJwt.getToken();
+        var jwtAuthenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        if (jwtAuthenticationToken == null) { //meaning that the BearerTokenAuthenticationFilter resolver couldn't get a token...
+            throw new AuthenticationCredentialsNotFoundException("Authentication Token not found!!!");
+        }
+        Jwt jwtToken = jwtAuthenticationToken.getToken();
         if (blackListedJwtTokenList.contains(jwtToken)) {
-            SecurityContextHolder.clearContext();
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.getWriter().write("Access Denied: Token is blacklisted");
-            response.getWriter().flush();
-            return;
+            throw new AccessDeniedException("Authentication Token is invalid");
         }
         doFilter(request, response, filterChain);
     }
