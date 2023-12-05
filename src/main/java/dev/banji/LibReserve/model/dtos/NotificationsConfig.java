bdf5e1a2b956e2bbee3d52472d2eeff8fefcf1c6
Banji <87107632+Banji-07@users.noreply.github.com>
2023-12-05 17:55:09 +0100

    refactor: notifications details to the NotificationsConfig class.

--- src/main/java/dev/banji/LibReserve/model/dtos/NotificationsConfig.java
+++ src/main/java/dev/banji/LibReserve/model/dtos/NotificationsConfig.java
@@ -1,5 +1,10 @@
 package dev.banji.LibReserve.model.dtos;
 
-public record NotificationsConfig(Boolean enabled, Boolean viaWeb, Boolean viaMail, String notificationSubject,
-                                  String notificationBody) {
+public record NotificationsConfig(Boolean enabled, Boolean viaWeb,
+                                  Boolean viaMail, String timeUpNotificationSubject,
+                                  String timeUpNotificationBody, String blackListNotificationSubject,
+                                  String blackListNotificationBody, String timeAlmostUpNotificationSubject,
+                                  String timeAlmostUpNotificationBody, String studentBlackListNotificationSubject,
+                                  String studentBlackListNotificationBody, String studentKickedOutNotificationSubject,
+                                  String studentKickedOutNotificationBody) {
 }
--- src/main/java/dev/banji/LibReserve/service/NotificationService.java
+++ src/main/java/dev/banji/LibReserve/service/NotificationService.java
@@ -2,15 +2,12 @@ package dev.banji.LibReserve.service;
 
 import dev.banji.LibReserve.model.dtos.EmailNotificationDto.BulkEmailNotificationDto;
 import dev.banji.LibReserve.model.dtos.EmailNotificationDto.SingleEmailNotificationDto;
-import dev.banji.LibReserve.model.dtos.WebNotificationDto;
+import dev.banji.LibReserve.model.dtos.NotificationsConfig;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
@@ -20,56 +17,48 @@ import java.util.List;
 public class NotificationService { //TODO validation also has to be done to ensure the place holders match a regex
     private final SimpMessagingTemplate messagingTemplate;
     private final EmailService emailService;
-    //TODO this should be injected from a file...
-    @Value("${library.properties.sendNotifications.notificationSubject}")
-    private String notificationSubject;
-    //TODO this should be injected from a file...
-    @Value("${library.properties.sendNotifications.notificationBody}")
-    private String notificationBody;
-    @Value("${library.properties.sendNotifications.viaWeb}")
-    private Boolean notifyViaWeb;
-    @Value("${library.properties.sendNotifications.viaMail}")
-    private Boolean notifyViaMail;
+    private final NotificationsConfig notificationsConfig;
+
 
     @PreAuthorize("hasAnyAuthority('SCOPE_LIBRARIAN')")
-    public void notifyAllUsersViaWeb(@Payload @Validated WebNotificationDto notification) { //TODO make sure this current logged in user is not included
-        messagingTemplate.convertAndSend("/notifications", notification.message());
+    public void notifyAllUsersViaWeb(String message) { //TODO make sure this current logged-in user is not included
+        messagingTemplate.convertAndSend("/notifications", message);
     }
 
     @PreAuthorize("hasAnyAuthority('SCOPE_LIBRARIAN')")
-    public void notifyAllLibrariansViaWeb(@Payload @Validated WebNotificationDto notification) {
-        messagingTemplate.convertAndSend("/notifications/librarians", notification.message());
+    public void notifyAllLibrariansViaWeb(String message) {
+        messagingTemplate.convertAndSend("/notifications/librarians", message);
     }
 
     @PreAuthorize("hasAnyAuthority('SCOPE_LIBRARIAN')")
-    public void notifyLibrarianViaWeb(@Payload @Validated WebNotificationDto notification) {
-        messagingTemplate.convertAndSendToUser(notification.userIdentifier().get(0), "/notifications/librarians", notification.message());
+    public void notifyLibrarianViaWeb(String emailAddress, String message) {
+        messagingTemplate.convertAndSendToUser(emailAddress, "/notifications/librarians", message);
     }
 
-    public void notifyLibrarian(WebNotificationDto notificationTimerDto, String emailAddress) {
-        if (notifyViaWeb) //notify via web
-            notifyLibrarianViaWeb(notificationTimerDto);
-        if (notifyViaMail) //notify via email
+    public void notifyLibrarian(String emailAddress, String message, String notificationSubject, String notificationBody) {
+        if (notificationsConfig.viaWeb()) //notify via web
+            notifyLibrarianViaWeb(emailAddress, message);
+        if (notificationsConfig.viaMail()) //notify via email
             this.emailService.sendEmailNotification(new SingleEmailNotificationDto(emailAddress, notificationSubject, notificationBody));
     }
 
-    public void notifyAllLibrarians(WebNotificationDto notificationTimerDto, List<String> emailAddressList) {
-        if (notifyViaWeb) //notify via web
-            notifyAllLibrariansViaWeb(notificationTimerDto);
-        if (notifyViaMail) //notify via email
-            this.emailService.sendEmailNotification(new BulkEmailNotificationDto(emailAddressList, notificationSubject, notificationBody));
+    public void notifyAllLibrarians(String webMessage, BulkEmailNotificationDto bulkEmailNotification) {
+        if (notificationsConfig.viaWeb()) //notify via web
+            notifyAllLibrariansViaWeb(webMessage);
+        if (notificationsConfig.viaMail()) //notify via email
+            this.emailService.sendEmailNotification(bulkEmailNotification);
     }
 
-    public void notifyAllUsers(WebNotificationDto notificationTimerDto, List<String> emailAddressList) {
-        if (notifyViaWeb) //notify via web
-            notifyAllUsersViaWeb(notificationTimerDto);
-        if (notifyViaMail) //notify via email
-            this.emailService.sendEmailNotification(new BulkEmailNotificationDto(emailAddressList, notificationSubject, notificationBody));
+    public void notifyAllUsers(BulkEmailNotificationDto bulkEmailNotification, String message) {
+        if (notificationsConfig.viaWeb()) //notify via web
+            notifyAllUsersViaWeb(message);
+        if (notificationsConfig.viaMail()) //notify via email
+            this.emailService.sendEmailNotification(bulkEmailNotification);
     }
 
     @PreAuthorize("hasAuthority('SCOPE_LIBRARIAN')")
-    private void notifyStudentViaWeb(@Payload @Validated WebNotificationDto notification) {
-        messagingTemplate.convertAndSendToUser(notification.userIdentifier().get(0), "/notifications/students", notification.message());
+    private void notifyStudentViaWeb(String matricNumber, String message) {
+        messagingTemplate.convertAndSendToUser(matricNumber, "/notifications/students", message);
     }
 
     @PreAuthorize("hasAuthority('SCOPE_LIBRARIAN')")
@@ -83,16 +72,16 @@ public class NotificationService { //TODO validation also has to be done to ensu
 
     @PreAuthorize("hasAuthority('SCOPE_LIBRARIAN')")
     //TODO validate the parameters...
-    public void notifyAllStudents(WebNotificationDto notificationTimerDto, List<String> emailAddressList) {
-        if (notifyViaWeb) //notify via web
-            notifyAllStudentsViaWeb(notificationTimerDto);
-        if (notifyViaMail) //notify via email
+    public void notifyAllStudents(List<String> emailAddressList, String notificationSubject, String notificationBody) {
+        if (notificationsConfig.viaWeb()) //notify via web
+            notifyAllStudentsViaWeb(notificationSubject, notificationBody);
+        if (notificationsConfig.viaMail()) //notify via email
             this.emailService.sendEmailNotification(new BulkEmailNotificationDto(emailAddressList, notificationSubject, notificationBody));
     }
 
     @PreAuthorize("hasAuthority('SCOPE_LIBRARIAN')")
-    private void notifyAllStudentsViaWeb(@Payload @Validated WebNotificationDto notification) {
-        messagingTemplate.convertAndSend("/notifications/students", notification.message());
+    private void notifyAllStudentsViaWeb(String notificationSubject, String notificationBody) {
+        messagingTemplate.convertAndSend("/notifications/students", notificationSubject + "\n" + notificationBody);
     }
 
 }
